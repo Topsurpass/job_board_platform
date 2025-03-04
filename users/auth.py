f187c9cb2c4db33cb69f0915adf379b75b432980
Topsurpass <temitopeabiodun685@gmail.com>
2025-03-04 15:31:35 +0100

    feat: returned user details together with access and refreshtoken as requested by FE

--- users/auth.py
+++ users/auth.py
@@ -54,12 +54,37 @@ class CustomTokenObtainPairView(TokenObtainPairView):
                 refresh["company_name"] = user.company_name
                 refresh["industry"] = user.industry
 
-            return Response({
-                'access_token': str(refresh.access_token),
-                'refresh_token': str(refresh),
-            })
+            user_data = {
+            "id": user.id,
+            "email": user.email,
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "is_staff": user.is_staff,
+            "is_superuser": user.is_superuser,
+            "is_active": user.is_active,
+            "role": user.role,
+            "phone": user.phone,
+            "groups": list(user.groups.values_list("name", flat=True)),
+            "permissions": list(user.user_permissions.values_list("codename", flat=True))
+        }
+
+        if user.role == "employer":
+            refresh["company_name"] = user.company_name
+            refresh["industry"] = user.industry
+            user_data["company_name"] = user.company_name
+            user_data["industry"] = user.industry
+
+        return Response(
+            {
+                "access_token": str(refresh.access_token),
+                "refresh_token": str(refresh),
+                "user": user_data,
+            },
+            status=status.HTTP_200_OK,
+        )
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 
